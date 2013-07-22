4f2cc791d058e07460240aacb9cecdcfc401bce3
jbowkett <james_bowkett@lyagushka.co.uk>
2013-07-22 22:20:16 +0100

    Added selenium and Chromium configuration into cukes setup

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-joxy
-====
+# Joxy
 
-A Java proxy server allowing for augmented browsing and URL/content filtering plugins
+Joxy is a Java proxy server allowing for augmented browsing and URL/content
+filtering plugins.  It is implemented in Java, using basic Java sockets and
+threads.
+
+## Cucumber notes
+
+Joxy uses [Cucumber-JVM][], and the Chrome Driver for Selenium.  Selenium
+requires a bridge to allow it to access the Chromium automation toolkit.
+This can be downloaded here:
+[https://code.google.com/p/selenium/wiki/ChromeDriver][]
+
+The path to the bridge executable can then be set in the system property
+`webdriver.chrome.driver` when running the scenarios.  This is currently done
+in Joxy's `pom.xml` in the line :
+
+`-Dwebdriver.chrome.driver=/Users/jbowkett/other/chromedriver/chromedriver`.
+
+ 
--- src/test/java/info/bowkett/joxy/stepdefs/ServerStartupStepDefs.java
+++ src/test/java/info/bowkett/joxy/stepdefs/ServerStartupStepDefs.java
@@ -1,9 +1,13 @@
 package info.bowkett.joxy.stepdefs;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,28 +19,43 @@ import cucumber.api.java.en.When;
 public class ServerStartupStepDefs {
 
 
+    protected WebDriver driver = new ChromeDriver();
+
+//    @Before
+//    public void setUp() {
+//        driver = new ChromeDriver();
+//    }
+
+
     @Given("^a joxy server is started$")
     public void a_joxy_server_is_started() throws Throwable {
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+//        throw new PendingException();
     }
 
     @Given("^a web browser configured to use joxy$")
     public void a_web_browser_configured_to_use_joxy() throws Throwable {
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+//        throw new PendingException();
     }
 
     @When("^I navigate to a website$")
     public void I_navigate_to_a_website() throws Throwable {
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        driver.get("http://www.google.co.uk");
     }
 
     @Then("^I will see that website in the browser$")
     public void I_will_see_that_website_in_the_browser() throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+      final String title = driver.getTitle();
+      System.out.println("title = " + title);
+      // Express the Regexp above with the code you wish you had
+//        throw new PendingException();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
     }
 
 

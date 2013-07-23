3981a933ef4cb009bcad020ca7f894f689339781
jbowkett <james_bowkett@lyagushka.co.uk>
2013-07-23 08:48:34 +0100

    create server class

---
+++ src/main/java/info/bowkett/joxy/Server.java
@@ -0,0 +1,21 @@
+package info.bowkett.joxy;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jbowkett
+ * Date: Jul 23, 2013
+ * Time: 8:45:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Server {
+  private final int port;
+
+  public Server(int port) {
+    this.port = port;
+  }
+
+  public void start() {
+
+    
+  }
+}

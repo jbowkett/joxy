93125cf26073bd44abeaa5f722c0e1f1688e378e
jbowkett <james_bowkett@lyagushka.co.uk>
2013-07-24 23:27:23 +0100

    refactored

--- src/main/java/info/bowkett/joxy/RequestParser.java
+++ src/main/java/info/bowkett/joxy/RequestParser.java
@@ -14,21 +14,26 @@ import java.util.StringTokenizer;
 public class RequestParser {
 
   public Request parseRequest(String request) {
-    final StringTokenizer lines = new StringTokenizer(request, "\n");
     final Map<String, String> values = new HashMap<String, String>();
-    while(lines.hasMoreElements()){
-      final String line = (String) lines.nextElement();
-      final int firstSpaceIndex = line.indexOf(' ');
-      if(firstSpaceIndex > -1){
-        final String key = line.substring(0, firstSpaceIndex);
-        final String value = line.substring(firstSpaceIndex +1);
-        values.put(clean(key), value);
-      }
+
+    final String lines [] = request.split("\n");
+    for (String line : lines) {
+      parseLine(values, line);
     }
+    
     return new Request(values.get("HOST"), values.get("PROXY-CONNECTION"),
       values.get("CACHE-CONTROL"), values.get("ACCEPT"), values.get("USER-AGENT"),
       values.get("ACCEPT-ENCODING"), values.get("ACCEPT-LANGUAGE"),
       values.get("COOKIE"));
+  }
+
+  private void parseLine(Map<String, String> values, String line) {
+    final int firstSpaceIndex = line.indexOf(' ');
+    if(firstSpaceIndex > -1){
+      final String key = line.substring(0, firstSpaceIndex);
+      final String value = line.substring(firstSpaceIndex +1);
+      values.put(clean(key), value);
+    }
   }
 
   /**

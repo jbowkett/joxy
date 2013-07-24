1ddd5dd730ba9a1f73b39b91625c5801f15455e8
jbowkett <james_bowkett@lyagushka.co.uk>
2013-07-24 22:08:17 +0100

    first checkin

---
+++ src/main/java/info/bowkett/joxy/RequestParser.java
@@ -0,0 +1,14 @@
+package info.bowkett.joxy;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jbowkett
+ * Date: Jul 24, 2013
+ * Time: 9:48:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RequestParser {
+  public Request parseRequest(String request) {
+
+  }
+}
